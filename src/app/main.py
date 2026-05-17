982ff12f3bb7b1c74d905da78313a78ec27a0b42
Ivan <moskvinuwu@gmail.com>
2026-05-17 19:55:32 +0800

    Added initialize database on app

--- src/app/main.py
+++ src/app/main.py
@@ -2,6 +2,8 @@
 
 from flask import Flask
 
+from library_db_core import Database
+
 from .routes import init_routes
 
 
@@ -10,9 +12,17 @@ def create_app() -> Flask:
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "dev-secret-key-change-in-production"
     
+    init_database()
+    
     init_routes(app)
     
     return app
 
+
+def init_database() -> None:
+    """Create database tables if they don't exist"""
+    db = Database().connect()
+    db.create_tables()
+    db.close()
 
 app = create_app()
